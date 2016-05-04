4e269a2c786f1c523c38b69b74625ca2e64319ea
TABISH ANSARI <tansari449@gmail.com>
2016-05-04 20:36:27 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,13 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
-  resources :quotes
+
   resources :quotes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+ root 'quotes#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
